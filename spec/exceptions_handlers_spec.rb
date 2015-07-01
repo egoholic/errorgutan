b7d517e2d50d16b2480a700bb698b04c58549a81
Vladimir Melnik <egotraumatic@gmail.com>
2015-07-01 17:27:13 +0300

    spec: ExceptionsHandlers 100% coverage with mutant

--- spec/exceptions_handlers_spec.rb
+++ spec/exceptions_handlers_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Errorgutan::ExceptionsHandlers do
-  let(:exception) { FakeException }
+  let(:exception_class) { FakeException }
   let(:handler) { Errorgutan::Handler.new { |e| "handler" } }
   let(:default_handler) { Errorgutan::Handler.new { |e| "default_handler" } }
 
@@ -14,11 +14,10 @@ RSpec.describe Errorgutan::ExceptionsHandlers do
       end
 
       context "when wrong arguments" do
-        context "when `default:` hanler is `nil` or not provided" do
-          it "raises an exception" do
-            expect { subject.new(default: nil) }.to raise_error(ArgumentError)
-            expect { subject.new }.to raise_error(ArgumentError)
-          end
+        it "raises ArgumentError" do
+          expect { subject.new default: nil }.to raise_error(ArgumentError)
+          expect { subject.new }.to raise_error(ArgumentError)
+          expect { subject.new default: Class.new }.to raise_error(ArgumentError)
         end
       end
     end
@@ -30,33 +29,26 @@ RSpec.describe Errorgutan::ExceptionsHandlers do
     describe "#handle" do
       context "when correct arguments" do
         it "assigns a handler for given exceptions" do
-          expect { subject.handle exception, with: handler }
-            .to change { subject[exception] }
+          expect { subject.handle exception_class, with: handler }
+            .to change { subject[exception_class] }
               .from(default_handler)
               .to(handler)
         end
       end
 
       context "when wrong arguments" do
-        context "when `exceptions` and binding `to:` are `nil` or not given" do
-          it "raises an exception" do
-            expect { subject.handle(nil, with: nil) }.to raise_error(ArgumentError)
-            expect { subject.handle}.to raise_error(ArgumentError)
-          end
-        end
-
-        context "when `exceptions` is `nil` or not given" do
-          it "raises an exception" do
-            expect { subject.handle(nil, with: handler) }.to raise_error(ArgumentError)
-            expect { subject.handle(with: handler) }.to raise_error(ArgumentError)
-          end
-        end
-
-        context "when binding `to:` is nil or not given" do
-          it "raises an exception" do
-            expect { subject.handle(exception, with: nil) }.to raise_error(ArgumentError)
-            expect { subject.handle(exception) }.to raise_error(ArgumentError)
-          end
+        it "raises ArgumentError" do
+          expect { subject.handle(nil, with: nil) }.to raise_error(ArgumentError)
+          expect { subject.handle}.to raise_error(ArgumentError)
+          expect { subject.handle(nil, with: handler) }.to raise_error(ArgumentError)
+          expect { subject.handle(with: handler) }.to raise_error(ArgumentError)
+          expect { subject.handle(exception_class, with: nil) }.to raise_error(ArgumentError)
+          expect { subject.handle(exception_class) }.to raise_error(ArgumentError)
+          expect { subject.handle Class.new, with: Class.new }.to raise_error(ArgumentError)
+          expect { subject.handle exception_class, with: Class.new }.to raise_error(ArgumentError)
+          expect { subject.handle Class.new, with: handler }.to raise_error(ArgumentError)
+          expect { subject.handle exception_class, Class.new, with: handler }.to raise_error(ArgumentError)
+          expect { subject.handle exception_class, nil, with: handler }.to raise_error(ArgumentError)
         end
       end
     end
@@ -65,24 +57,24 @@ RSpec.describe Errorgutan::ExceptionsHandlers do
       context "when correct arguments" do
         context "when has defined handler" do
           it "returns handler" do
-            subject.handle(exception, with: handler)
+            subject.handle(exception_class, with: handler)
 
-            expect(subject[exception]).to eq handler
+            expect(subject[exception_class]).to eq handler
           end
         end
 
         context "when has no defined handler" do
           it "returns default handler" do
-            expect(subject[exception]).to eq default_handler
+            expect(subject[exception_class]).to eq default_handler
           end
         end
       end
 
       context "when wrong arguments" do
-        context "when `exception` is `nil` or not given" do
-          it "raises an exception" do
-            expect { subject[] }.to raise_error(ArgumentError)
-          end
+        it "raises an exception" do
+          expect { subject[] }.to raise_error(ArgumentError)
+          expect { subject[nil] }.to raise_error(ArgumentError)
+          expect { subject[Class.new] }.to raise_error(ArgumentError)
         end
       end
     end
